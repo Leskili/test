62176bd6efca8e84ef9b252f0f6dfd8fa1bac8db
Leskili <36904342+Leskili@users.noreply.github.com>
2018-02-27 21:13:01 -0800

    Add files via upload

---
+++ notepad/notepad/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmSaveAs = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsmOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PasteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.statWordCount = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.statSentenceCount = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.txtBox = New System.Windows.Forms.TextBox()
+        Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.FindToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FindNextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(772, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmSave, Me.tsmSaveAs, Me.ToolStripMenuItem1, Me.tsmOpen, Me.ToolStripMenuItem2, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'tsmSave
+        '
+        Me.tsmSave.Name = "tsmSave"
+        Me.tsmSave.Size = New System.Drawing.Size(114, 22)
+        Me.tsmSave.Text = "&Save"
+        '
+        'tsmSaveAs
+        '
+        Me.tsmSaveAs.Name = "tsmSaveAs"
+        Me.tsmSaveAs.Size = New System.Drawing.Size(114, 22)
+        Me.tsmSaveAs.Text = "Save &As"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(111, 6)
+        '
+        'tsmOpen
+        '
+        Me.tsmOpen.Name = "tsmOpen"
+        Me.tsmOpen.Size = New System.Drawing.Size(114, 22)
+        Me.tsmOpen.Text = "&Open"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(111, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CutToolStripMenuItem, Me.CopyToolStripMenuItem, Me.PasteToolStripMenuItem, Me.ToolStripMenuItem3, Me.FindToolStripMenuItem, Me.FindNextToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'CutToolStripMenuItem
+        '
+        Me.CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        Me.CutToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.CutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CutToolStripMenuItem.Text = "&Cut"
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CopyToolStripMenuItem.Text = "C&opy"
+        '
+        'PasteToolStripMenuItem
+        '
+        Me.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        Me.PasteToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.V), System.Windows.Forms.Keys)
+        Me.PasteToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PasteToolStripMenuItem.Text = "&Paste"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.statWordCount, Me.ToolStripStatusLabel2, Me.statSentenceCount})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 474)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(772, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(78, 17)
+        Me.ToolStripStatusLabel1.Text = "Word Count: "
+        '
+        'statWordCount
+        '
+        Me.statWordCount.Name = "statWordCount"
+        Me.statWordCount.Padding = New System.Windows.Forms.Padding(8, 0, 0, 0)
+        Me.statWordCount.Size = New System.Drawing.Size(8, 17)
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Padding = New System.Windows.Forms.Padding(16, 0, 0, 0)
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(113, 17)
+        Me.ToolStripStatusLabel2.Text = "Sentence Count: "
+        '
+        'statSentenceCount
+        '
+        Me.statSentenceCount.Name = "statSentenceCount"
+        Me.statSentenceCount.Padding = New System.Windows.Forms.Padding(8, 0, 0, 0)
+        Me.statSentenceCount.Size = New System.Drawing.Size(8, 17)
+        '
+        'txtBox
+        '
+        Me.txtBox.Location = New System.Drawing.Point(0, 27)
+        Me.txtBox.Multiline = True
+        Me.txtBox.Name = "txtBox"
+        Me.txtBox.Size = New System.Drawing.Size(772, 444)
+        Me.txtBox.TabIndex = 2
+        '
+        'ToolStripMenuItem3
+        '
+        Me.ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        Me.ToolStripMenuItem3.Size = New System.Drawing.Size(149, 6)
+        '
+        'FindToolStripMenuItem
+        '
+        Me.FindToolStripMenuItem.Name = "FindToolStripMenuItem"
+        Me.FindToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.FindToolStripMenuItem.Text = "&Find"
+        '
+        'FindNextToolStripMenuItem
+        '
+        Me.FindNextToolStripMenuItem.Name = "FindNextToolStripMenuItem"
+        Me.FindNextToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.FindNextToolStripMenuItem.Text = "Find &Next"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(772, 496)
+        Me.Controls.Add(Me.txtBox)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Notepad"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents tsmSave As ToolStripMenuItem
+    Friend WithEvents tsmSaveAs As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents tsmOpen As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents statWordCount As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents statSentenceCount As ToolStripStatusLabel
+    Friend WithEvents txtBox As TextBox
+    Friend WithEvents ToolStripMenuItem2 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripSeparator
+    Friend WithEvents FindToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FindNextToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ notepad/notepad/Form1.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+    Dim defaultSavePath As String = ""
+
+    Private Sub tsmSave_Click(sender As Object, e As EventArgs) Handles tsmSave.Click
+        'will automatically write txtbox.text to the file, unless no file has been saved to yet, otherwise does save as
+        If (defaultSavePath.Length > 0) Then    'if there is a default save file, then save to it
+            save(defaultSavePath)
+        Else
+            tsmSaveAs_Click(sender, e)
+        End If
+    End Sub
+
+    Private Sub save(path As String)
+        Using stream As New IO.StreamWriter(path)
+            stream.WriteLine(txtBox.Text)
+            stream.Close()
+        End Using
+    End Sub
+
+    Private Sub tsmSaveAs_Click(sender As Object, e As EventArgs) Handles tsmSaveAs.Click
+        Dim dlgSave = New SaveFileDialog
+        dlgSave.Title = "Save File"
+        dlgSave.Filter = "Text File | *.txt"
+        dlgSave.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        dlgSave.ShowDialog()
+        If (dlgSave.FileName.Length > 0) Then
+            save(dlgSave.FileName)
+        Else
+            MessageBox.Show("File not saved.", "Error Saving", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub tsmOpen_Click(sender As Object, e As EventArgs) Handles tsmOpen.Click
+        Dim dlgOpen = New OpenFileDialog
+        dlgOpen.Title = "Save File"
+        dlgOpen.Filter = "Text File | *.txt"
+        dlgOpen.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        dlgOpen.ShowDialog()
+        If (dlgOpen.FileName.Length > 0) Then
+            Dim myFile = IO.File.OpenText(dlgOpen.FileName)
+            txtBox.Text = myFile.ReadToEnd
+            myFile.Close()
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtBox_TextChanged(sender As Object, e As EventArgs) Handles txtBox.TextChanged
+        'updates word and sentence counts 
+        Dim lstWords As List(Of String) = Split(txtBox.Text).ToList()   'creates a list of strings delimited by " "
+        lstWords.RemoveAll(Function(value As String) value = "")    'removes from the list all empty strings
+        statWordCount.Text = lstWords.Count.ToString()
+
+        Dim lstSentences As List(Of String) = Split(txtBox.Text, ".").ToList()  'same as above, but with period
+        lstSentences.RemoveAll(Function(value As String) value = "")
+        statSentenceCount.Text = lstSentences.Count.ToString()
+    End Sub
+
+    Private Sub FindToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FindToolStripMenuItem.Click
+        findString(InputBox("Search for a string.", "Find"))    'searches for a string using an input box as the search key
+    End Sub
+
+    Private Sub FindNextToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FindNextToolStripMenuItem.Click
+        findString()    'searches for a string using the most recently used search key
+    End Sub
+
+    Private Sub findString(Optional ByVal strFind As String = "")
+        Dim text As String = txtBox.Text
+        Static key As String   'static string, used as search key
+        Static intLastStart As Integer = 1  'static int, tracks last index of found string
+
+        If (strFind <> "") Then
+            key = strFind  'if no argument passed, use most recent search key
+        End If
+
+        If (key = "") Then 'if there is no search key, get one from the user (happens when find next before find)
+            key = InputBox("Search for a string.", "Find")
+        End If
+
+        Dim index As Integer = InStr(intLastStart, text, key)  'gets the index of the first occurrence of the key term
+        If (index > 0) Then 'selects the key term if it exists in the txtbox
+            txtBox.SelectionStart = index - 1
+            txtBox.SelectionLength = key.Length
+            intLastStart = index + key.Length   'updates the index to avoid finding the same part of the string over and over
+        End If
+
+    End Sub
+End Class
